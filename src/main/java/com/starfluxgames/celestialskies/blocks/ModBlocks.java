a342b09bd8d4f7d70dd3b862c53c978015c15e12
Lachlan Leone <starfluxgames@gmail.com>
2025-03-30 02:07:05 +1100

    Added light emission to Mysticite Ore

--- src/main/java/com/starfluxgames/celestialskies/blocks/ModBlocks.java
+++ src/main/java/com/starfluxgames/celestialskies/blocks/ModBlocks.java
@@ -18,8 +18,8 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(CelestialSkies.MOD_ID);
 
-    public static final DeferredBlock<Block> MYSTICITE_ORE = registerBlock("mysticite_ore", () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
-    public static final DeferredBlock<Block> DEEPSLATE_MYSTICITE_ORE = registerBlock("deepslate_mysticite_ore", () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of().strength(5f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> MYSTICITE_ORE = registerBlock("mysticite_ore", () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE).lightLevel(value -> 5)));
+    public static final DeferredBlock<Block> DEEPSLATE_MYSTICITE_ORE = registerBlock("deepslate_mysticite_ore", () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of().strength(5f).requiresCorrectToolForDrops().sound(SoundType.STONE).lightLevel(value -> 5)));
     public static final DeferredBlock<Block> RAW_MYSTICITE_BLOCK = registerBlock("raw_mysticite_block", () -> new Block(BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
